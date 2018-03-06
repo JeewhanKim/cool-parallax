b658eaa72e050e2338dadecf9f5d406f16c4985a
Jeewhan Kim <awryong@gmail.com>
2018-03-06 17:50:06 -0500

    updates

--- src/lib/cool-parallax.js
+++ src/lib/cool-parallax.js
@@ -1,2 +1,22 @@
 // alert('jee test');
+const win = $(window)
+const doc = $(document)
 
+const genNav = () => {
+
+}
+
+const initialStyles = () => {
+  // alert('init');
+}
+
+const scrollDetect = () => {
+  console.log('scrollDetect')
+}
+
+genNav()
+initialStyles()
+scrollDetect()
+
+win.resize( _ =>  { initialStyles() });
+win.scroll( _ =>  { scrollDetect() });
